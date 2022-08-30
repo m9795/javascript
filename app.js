8a3fe8c17fbb698c178adfedc409540cd3639c0a
m9795 <106650955+m9795@users.noreply.github.com>
2022-08-31 03:51:46 +0900

    [Add]アラートメッセージの表示練習

---
+++ app.js
@@ -0,0 +1,68 @@
+// アラートメッセージ表示
+// alert("Hello World");
+
+
+// 変数定義
+// let hello = "Hellow World";
+// alert(hello);
+
+// 文字列つなげる
+// alert("Hello" + "World");
+
+// 変数定義してつなげる
+// let str1 = "Hello";
+// let str2 = "World!";
+// alert(str1 + str2);
+
+// 変数定義して条件分岐
+// let orange = 100;
+// let apple = 120;
+
+// if(orange < apple) {
+//   alert("みかんの値段がりんごより安い");
+// } else if(orange == apple){
+//   alert("みかんとりんごが同じ値段");
+// } else{
+//   alert("みかんの値段がりんごより高い");
+// }
+
+// 変数定義して繰り返し処理1
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert("2をかけ続けて" + max + "を超えるのに必要だった回数は" + count + "回です");
+
+
+// 変数定義して必要な回数繰り返し処理をする
+// let i;
+// let num = 0;
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert("1から10まで足し算した結果は" + num + "です");
+
+
+// alert(5 - 2);
+// alert(192 * 333);
+// alert(1941566 / 2);
+
+// 練習
+let money = 150
+let orange = 1000
+let diff = (money - orange)
+
+if (diff >= 0){
+  alert
+  ("所持金は" + money + "円、みかんは" + orange + "円です。"
+  + "残金は" + diff + "円です")
+} else{
+  alert("所持金は" + money + "円、みかんは" + orange + "円です。"
+  + "みかんは買えません")
+}
