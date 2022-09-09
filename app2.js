02f0087c26452984a9cbdb7da44d125fad009b70
m9795 <m.ogawa9795@gmail.com>
2022-09-09 23:33:10 +0000

    [Add]アニメーションの追加

--- app2.js
+++ app2.js
@@ -1,6 +1,11 @@
 $(function(){
-  $('.box1').css({
-    'background-color': '#0000FF',
-    'height': '100px'
-  });
+  // div要素のスタイル変更
+  // $('.box1').css({
+  //   'background-color': '#0000FF',
+  //   'height': '100px'
+  // });
+
+  // アニメーションの追加
+  $('.box1').slideDown();
+  // $('.box1').slideUp();
 });
