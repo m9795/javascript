accee6e1d5bb717b1da6e00a1a2b3b2264caba4e
m9795 <m.ogawa9795@gmail.com>
2022-09-10 00:22:14 +0000

    [Add]確認問題

--- app2.js
+++ app2.js
@@ -1,4 +1,4 @@
-$(function(){
+// $(function(){
   // div要素のスタイル変更
   // $('.box1').css({
   //   'background-color': '#0000FF',
@@ -6,6 +6,25 @@ $(function(){
   // });
 
   // アニメーションの追加
-  $('.box1').slideDown();
+  // $('.box1').slideDown();
   // $('.box1').slideUp();
+
+  // 非表示要素をjQueryで表示させる
+    // $('.box1').show();
+    // $('.box1').css({'background-color': '#0000FF'});
+
+  // 要素の非表示
+  // $('.box1').hide();
+// });
+
+
+// 確認問題
+$(function(){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
