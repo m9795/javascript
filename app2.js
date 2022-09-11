f6a06693e0bdd99923bb31ed9cf21eacf151e94f
m9795 <m.ogawa9795@gmail.com>
2022-09-11 04:22:45 +0000

    [Add]マウスオーバーイベント

--- app2.js
+++ app2.js
@@ -1,30 +1,8 @@
-// $(function(){
-  // div要素のスタイル変更
-  // $('.box1').css({
-  //   'background-color': '#0000FF',
-  //   'height': '100px'
-  // });
-
-  // アニメーションの追加
-  // $('.box1').slideDown();
-  // $('.box1').slideUp();
-
-  // 非表示要素をjQueryで表示させる
-    // $('.box1').show();
-    // $('.box1').css({'background-color': '#0000FF'});
-
-  // 要素の非表示
-  // $('.box1').hide();
-// });
-
-
-// 確認問題
 $(function(){
-  $('.box1').slideDown(function (){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp();
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color': '#0000FF'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color': '#FF0000'});
   });
 });
