46fcfcf311778b1893866546b77d2a117e9ca057
m9795 <m.ogawa9795@gmail.com>
2022-09-11 05:13:30 +0000

    [Add]マウスクリックイベント

--- app2.js
+++ app2.js
@@ -1,8 +1,19 @@
+// class属性の追加と解除
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+// マウスクリックイベント
 $(function(){
-  $('.box1').mouseover(function(){
-    $('.box1').css({'background-color': '#0000FF'});
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
   });
   $('.box1').mouseout(function(){
-    $('.box1').css({'background-color': '#FF0000'});
+    $('.box1').removeClass('box1-ext');
   });
 });
